51511afbc709dfaf8b5215be1e1e9f119ab58701
George Kyrylenko <dinozawrua@gmail.com>
2023-10-01 13:44:07 +0200

    init

---
+++ CoreDataUnitTests/CoreDataUnitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataUnitTestsApp.swift
+//  CoreDataUnitTests
+//
+//  Created by George Kyrylenko on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataUnitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
